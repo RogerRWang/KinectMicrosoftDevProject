cb04312ee2557ae200370860911577434ff35d61
RogerRWang <rogerrw@umich.edu>
2014-11-24 20:52:23 -0500

    Upload All
    upload from local to git

---
+++ KinectMicrosoftDevChallenge/CmdMessenger/CSharp/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main()
+        {
+            // mimics Arduino calling structure
+            var receive = new Receive { RunLoop = true };
+            receive.Setup();
+            while (receive.RunLoop) receive.Loop();
+            receive.Exit();
+        }
+
+    }
+}
